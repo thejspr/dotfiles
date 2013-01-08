f04d1f9c6eba2c5e13b9b0e70bf32a083a11e0ff
Jesper Kjeldgaard <thejspr@gmail.com>
2013-01-08 12:20:58 +0000

    Delete dotjs dir

--- js/product.event.ly.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('sidebar').style.width = '6%';
-document.getElementById('content').style.width = '90%';
--- js/www.trendsonline.dk.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function(){
-  jQuery('.js .tmce-active .wp-editor-area').css('color', '#000');
-})
