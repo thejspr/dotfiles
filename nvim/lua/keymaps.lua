33a0a060064f6642d750a725f0bd07ca739781f1
Jesper Kjeldgaard <thejspr@gmail.com>
2023-03-13 10:00:48 +0100

    config neotree

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,5 +1,9 @@
+-- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>t', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+
+-- Neotree
+vim.keymap.set('n', 'tt', ':Neotree<CR>', {})
