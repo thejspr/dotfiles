8ce25fe348a03b002de1b040153580c458d18dc1
Jesper Kjeldgaard <thejspr@gmail.com>
2023-07-31 09:13:07 +0200

    remove fluffy plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -80,56 +80,6 @@ return require('packer').startup(function(use)
     end
   }
 
-  -- notifications
-  use {
-    "rcarriga/nvim-notify",
-    keys = {
-      {
-        "<leader>un",
-        function()
-          require("notify").dismiss({ silent = true, pending = true })
-        end,
-        desc = "Dismiss all Notifications",
-      },
-    },
-    opts = {
-      timeout = 3000,
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.75)
-      end,
-    },
-    init = function()
-      -- when noice is not enabled, install notify on VeryLazy
-      local Util = require("lazyvim.util")
-      if not Util.has("noice.nvim") then
-        Util.on_very_lazy(function()
-          vim.notify = require("notify")
-        end)
-      end
-    end
-  }
-
-  -- better vim.ui
-  use {
-    "stevearc/dressing.nvim",
-    lazy = true,
-    init = function()
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.select = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.select(...)
-      end
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.input = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.input(...)
-      end
-    end
-  }
-
   -- bufferline
   use {
     "akinsho/bufferline.nvim",
@@ -162,12 +112,5 @@ return require('packer').startup(function(use)
         },
       },
     },
-  }
-
-  use {
-    'VonHeikemen/fine-cmdline.nvim',
-    requires = {
-      {'MunifTanjim/nui.nvim'}
-    }
   }
 end)
