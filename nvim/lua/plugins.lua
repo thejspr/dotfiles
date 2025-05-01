b11c5f54294bd61dc96c2780f676fe6005cb7c19
Jesper Kjeldgaard <Jesper Kjeldgaard>
2025-05-01 12:42:18 +0200

    Gitblame and lualine

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -89,19 +89,30 @@ return require('lazy').setup({
       'nvim-lua/plenary.nvim',
     },
   },
+  {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+        enabled = true,  -- if you want to enable the plugin
+        message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+        date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+        virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+        message_when_not_committed = "", -- message when the file is not committed
+    },
+  },
 
   -- AI
-  {
-    "zbirenbaum/copilot.lua",
-    lazy = false,
-    event = "InsertEnter",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    config = function()
-      require("copilot").setup({})
-    end,
-  },
-  -- 'github/copilot.vim',
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   lazy = false,
+  --   event = "InsertEnter",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   config = function()
+  --     require("copilot").setup({})
+  --   end,
+  -- },
+  'github/copilot.vim',
   -- {
   --   "CopilotC-Nvim/CopilotChat.nvim",
   --   dependencies = {
@@ -254,7 +265,14 @@ return require('lazy').setup({
 
   -- UI & Themes
 
-  'itchyny/lightline.vim',
+  -- 'itchyny/lightline.vim',
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup()
+    end
+  },
   -- 'projekt0n/github-nvim-theme',
   'catppuccin/nvim',
 
