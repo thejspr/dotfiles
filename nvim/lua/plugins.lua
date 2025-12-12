dbe3a6c5a53dffa0278829afdfd0917eb03da654
Jesper Kjeldgaard <jk@clearhaus.com>
2025-12-12 11:58:35 +0100

    git blame and etc

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -260,6 +260,17 @@ return require('lazy').setup({
   },
 
   {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+      enabled = false,  -- if you want to enable the plugin
+      message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+      date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+      virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+      message_when_not_committed = "", -- message when the file is not committed
+    } 
+  },
+  {
     'lewis6991/gitsigns.nvim',
     event = 'VeryLazy',
     config = function()
